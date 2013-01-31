116c4e6ffb3677295b29a22d2761e0d0aa429329
David Anderson <davea42@earthlink.net>
2013-01-31 15:55:05 -0800

    Just fixing charfix.sh to be easier to do in stages. Not part of the .tex.  aiding utf-8 removal.

--- dwarf5/tools/charfix.sh
+++ dwarf5/tools/charfix.sh
@@ -1,6 +1,23 @@
 
-for i in ../latexdoc/*.tex
-do
-    python charfix.py $i
-    mv $i.out $i
-done
+m() {
+    python charfix.py  ../latexdoc/$1
+    mv ../latexdoc/$1.out ../latexdoc/$1
+}
+#m attributesbytag.tex	       
+#m foreword4.1.tex 
+m compression.tex		       
+exit 0
+m foreword.tex 
+m copyright.tex		       
+m generaldescription.tex dataobject.tex		       
+m gnulicense.tex 
+m datarepresentation.tex	       
+m introduction.tex 
+m debugsectionrelationships.tex  
+m otherdebugginginformation.tex 
+m dwarf5.tex		       
+m programscope.tex 
+m encodingdecoding.tex	       
+m sectionversionnumbers.tex 
+m examples.tex		       
+m typeentries.tex
